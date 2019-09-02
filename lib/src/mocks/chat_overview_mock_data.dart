196a6f23b9f953daac1ac865cd3b0505d52831e4
jeroenouw <jouwehand@anwb.nl>
2019-09-02 21:32:41 +0200

    Added chat model and updated chat overveiw mock data

--- lib/src/mocks/chat_overview_mock_data.dart
+++ lib/src/mocks/chat_overview_mock_data.dart
@@ -1,40 +1,52 @@
-class ChatModel {
-  final String name;
-  final String message;
-  final String time;
-
-  ChatModel({this.name, this.message, this.time});
-}
+import 'package:social/src/models/chat_model.dart';
 
 List<ChatModel> chatOverviewMockData = [
   ChatModel(
+      chatId: 4132134,
       name: "Peter R",
       message: "Hey Flutter, You are so amazing !",
       time: "15:30",
   ),
   ChatModel(
+      chatId: 9132134,
       name: "John Doe",
       message: "Hey I have hacked whatsapp!",
       time: "17:30",
   ),
   ChatModel(
+      chatId: 7132134,
       name: "Mike Ross",
       message: "Wassup !",
       time: "5:00",
   ),
   ChatModel(
+      chatId: 4134134,
       name: "Bob",
       message: "I'm good!",
       time: "10:30",
   ),
   ChatModel(
+      chatId: 4132135,
       name: "Barry Allen",
       message: "I'm the fastest man alive!",
       time: "12:30",
   ),
   ChatModel(
+      chatId: 3132134,
       name: "Joe West",
       message: "Hey Flutter, You are so cool !",
       time: "15:30",
+  ),
+  ChatModel(
+      chatId: 2132134,
+      name: "Dave",
+      message: "I'm the fastest man alive!",
+      time: "12:35",
+  ),
+  ChatModel(
+      chatId: 1132134,
+      name: "William East",
+      message: "Hey Flutter, You are so cool !",
+      time: "12:37",
   ),
 ];
---
+++ lib/src/models/chat_model.dart
@@ -0,0 +1,8 @@
+class ChatModel {
+  final int chatId;
+  final String name;
+  final String message;
+  final String time;
+
+  ChatModel({this.chatId, this.name, this.message, this.time});
+}
